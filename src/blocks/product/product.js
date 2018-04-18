07bd56a6e944a0f8affa6f9beb1c52ea1b9e4ee1
tayurus <tayurus@gmail.com>
2018-04-19 01:02:21 +0300

    main page was completed

---
+++ src/blocks/product/product.js
@@ -0,0 +1,17 @@
+//обработчик нажатия на размер товара
+$(".product__size").click(function() {
+    //удаляем активный класс у предыдущего выбранного размера
+    $(this).parent().find('.product__size_active').removeClass("product__size_active");
+    //добавляем активный класс тому размеру, на который нажали
+    $(this).addClass("product__size_active");
+});
+
+//обработчик нажатия на превью товара
+$(".product__previews-item").click(function() {
+    //удаляем активный класс у предыдущего выбранного превью
+    $(this).parent().find('.product__previews-item_active').removeClass("product__previews-item_active");
+    //добавляем активный класс тому превью, на которое нажали
+    $(this).addClass("product__previews-item_active");
+    //устанавливаем выбранное превью в качестве основного изображения товара
+    $(this).parent().parent().find('.product__img').attr("src", $(this).attr("src"));
+});
