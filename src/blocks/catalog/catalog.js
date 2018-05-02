7d27c448f7758f4182f06a0577209664d2ca1fc3
tayurus <tayurus@gmail.com>
2018-05-02 20:05:52 +0300

    fixes

---
+++ src/blocks/catalog/catalog.js
@@ -0,0 +1,4 @@
+$(".catalog__pagination-item").click(function() {
+    $(".catalog__pagination-item").removeClass("catalog__pagination-item_active");
+    $(this).addClass("catalog__pagination-item_active");
+})
